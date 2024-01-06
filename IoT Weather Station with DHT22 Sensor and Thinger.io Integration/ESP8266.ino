c9eef176d4371dfb6929ad5c1ccf75a21a417b6a
Michail Gouvalaris <int02246@uoi.gr>
2024-01-06 17:40:54 +0200

    Initial commit

---
+++ IoT Weather Station with DHT22 Sensor and Thinger.io Integration/ESP8266.ino
@@ -0,0 +1,83 @@
+#define THINGER_SERIAL_DEBUG
+
+#include <ThingerESP8266.h>
+#include "arduino_secrets.h"
+#include "DHT.h"
+#include <LiquidCrystal_I2C.h>
+
+//dht Config
+#define DHTPIN D3
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+
+const int numReadings = 60; // Number of readings to average over (1 reading per second)
+float temperatureReadings[numReadings];
+int readingIndex = 0;
+
+void setup()
+{
+  // open serial for monitoring
+  Serial.begin(115200);
+  Serial.begin(9600);
+  // set builtin led as output
+  pinMode(LED_BUILTIN, OUTPUT);
+  dht.begin();
+  // add WiFi credentials
+  thing.add_wifi(SSID, SSID_PASSWORD);
+
+  // digital pin control example (i.e. turning on/off a light, a relay, configuring a parameter, etc)
+  thing["led"] << digitalPin(LED_BUILTIN);
+
+  // resource output example (i.e. reading a sensor value)
+  thing["dht22"] >> [](pson &out) {
+    out["humidity"] = dht.readHumidity();
+    out["celsius"] = dht.readTemperature();
+    out["avg"] = getAverageTemperature();
+  };
+
+  // more details at http://docs.thinger.io/arduino/
+}
+
+void loop()
+{
+  thing.handle();
+  delay(1000); // Delay for 1 second
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t))
+  {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Store the temperature reading in the array
+  temperatureReadings[readingIndex] = t;
+  readingIndex = (readingIndex + 1) % numReadings;
+
+  lcd.init();
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print(F("Humidity: "));
+  lcd.print(h);
+  lcd.setCursor(0, 1);
+  lcd.print(F("Temp.: "));
+  lcd.print(t);
+}
+
+float getAverageTemperature()
+{
+  float total = 0;
+  for (int i = 0; i < numReadings; i++)
+  {
+    total += temperatureReadings[i];
+  }
+  return total / numReadings;
+}
---
+++ IoT Weather Station with DHT22 Sensor and Thinger.io Integration/arduino_secrets.h
@@ -0,0 +1,6 @@
+#define USERNAME "ninnan"
+#define DEVICE_ID "ESP12E"
+#define DEVICE_CREDENTIAL "0Q!H?!YIcF?IH5%Q"
+
+#define SSID "Ninnan"
+#define SSID_PASSWORD "mixalis789101112"
