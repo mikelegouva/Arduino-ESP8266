1f1a6d4071e2c5d0cb7f9932d7e5c3d6192a3f17
Michail Gouvalaris <int02246@uoi.gr>
2024-01-23 17:08:11 +0200

    Change led d4

--- IoT Weather Station with DHT22 Sensor and Thinger.io Integration/ESP8266/ESP8266.ino
+++ IoT Weather Station with DHT22 Sensor and Thinger.io Integration/ESP8266/ESP8266.ino
@@ -22,13 +22,13 @@ void setup()
   Serial.begin(115200);
   Serial.begin(9600);
   // set builtin led as output
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(D4, OUTPUT);
   dht.begin();
   // add WiFi credentials
   thing.add_wifi(SSID, SSID_PASSWORD);
 
   // digital pin control example (i.e. turning on/off a light, a relay, configuring a parameter, etc)
-  thing["led"] << digitalPin(LED_BUILTIN);
+  thing["led"] << digitalPin(D4);
 
   // resource output example (i.e. reading a sensor value)
   thing["dht22"] >> [](pson &out) {
